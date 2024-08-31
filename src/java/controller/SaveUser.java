aacebdd726e3ab93d577e91664b882c5faaceabd
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2024-08-31 11:51:29 +0530

    files updated

---
+++ src/java/controller/SaveUser.java
@@ -0,0 +1,25 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.HibernateUtil;
+import org.hibernate.Hibernate;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+@WebServlet(name = "SaveUser", urlPatterns = {"/SaveUser"})
+public class SaveUser extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        SessionFactory sessioFactory = HibernateUtil.getSessionFactory();
+        Session session = sessioFactory.openSession();
+
+    }
+
+}
