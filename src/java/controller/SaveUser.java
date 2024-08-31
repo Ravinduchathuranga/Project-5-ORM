44c230c86bbeabf19851d508e7793d20a2493ad4
Ravindu Chathuranga Dasanayaka <ravicdasanayaka@gmail.com>
2024-08-31 11:55:27 +0530

    bean updated

--- src/java/controller/SaveUser.java
+++ src/java/controller/SaveUser.java
@@ -19,7 +19,7 @@ public class SaveUser extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         SessionFactory sessioFactory = HibernateUtil.getSessionFactory();
         Session session = sessioFactory.openSession();
-
+        session.save(req);
     }
 
 }
---
+++ src/java/model/User.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ravinduchathuranga
+ */
+public class User implements Serializable {
+
+    private int id;
+    private String name;
+    private String mobile;
+
+    public User() {}
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the mobile
+     */
+    public String getMobile() {
+        return mobile;
+    }
+
+    /**
+     * @param mobile the mobile to set
+     */
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+}
